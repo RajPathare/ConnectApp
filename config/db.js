0edb67946312ed937fd64a12037cd2fbc94b51fe
RajPathare <rajgpathare@gmail.com>
2020-12-19 17:08:04 +0530

    mongo added

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = process.env.mongoURI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected!');
+    }
+    catch (e) {
+        console.log(e.message);
+        // exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
