15245c07c5cb2bd114fead1cd077d5123cc45cc8
RajPathare <rajgpathare@gmail.com>
2021-01-11 20:32:43 +0530

    login/register page added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
+// we're using functional components here so we'll be using hooks
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onInputChange = (e) => {
+        setFormData({
+            // e.target.name can be username,email,password
+            ...formData, [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
-        <div>
-            Login!
+        <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead"><i className="fas fa-user"></i> Sign into your account</p>
+      <form className="form" onSubmit={(e)=> onSubmit(e)}>
+       
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={(e)=> onInputChange(e)} required />
         </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} onChange={(e)=> onInputChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+        </Fragment>
     )
 }
 
