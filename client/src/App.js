0b7bc07e79751cc546c47c7534ba080928f162d3
RajPathare <rajgpathare@gmail.com>
2021-01-12 22:25:59 +0530

    private route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
+
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import Alert from './components/layout/Alert';
 
@@ -33,6 +36,7 @@ const App = (props) => {
                     <Switch>
                         <Route exact path='/login' component={Login} />
                         <Route exact path='/register' component={Register} />
+                        <PrivateRoute exact path='/dashboard' component={Dashboard} />
                     </Switch>
                 </section>
             </Fragment>
