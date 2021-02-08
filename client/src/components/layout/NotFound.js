858a111c6958700460e1ec2eea06f6e10e41de0f
RajPathare <rajgpathare@gmail.com>
2021-02-08 23:18:51 +0530

    notFound added

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <h1 className="x-large text-primary">
+                <i className="fas fa-exclamation-triangle"></i> Page not found!
+            </h1>
+        </Fragment>
+    )
+}
+
+NotFound.propTypes = {
+
+}
+
+export default NotFound;
