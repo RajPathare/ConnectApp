3306216133cff1d2b8897b2557eff1adb7e28bfe
RajPathare <rajgpathare@gmail.com>
2021-02-08 22:38:17 +0530

    deployment changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 app.use(express.json({ extended:false }));
@@ -9,12 +10,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res)=> {
-    res.json({
-        "message": "API running!"
-    });
-})
-
 // define routes
 
 app.use('/api/users', require('./routes/api/users'));
@@ -22,6 +17,17 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Server static assets in prod
+
+if(process.env.NODE_ENV === 'production') {
+    
+    // static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
