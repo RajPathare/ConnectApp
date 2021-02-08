e451e40aad3290f910557398c524e0a71fc303e8
RajPathare <rajgpathare@gmail.com>
2021-02-08 15:41:41 +0530

    like/unlike a post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 
@@ -12,6 +12,41 @@ export const getPosts = () => {
             const res = await axios.get('/api/posts');
 
             dispatch({ type: GET_POSTS, payload: res.data });
+        }
+        catch (err) {
+            dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+        }
+    }
+
+}
+
+
+// Add a like
+
+export const addLike = (postId) => {
+    return async (dispatch) => {
+
+        try {
+            const res = await axios.put(`/api/posts/like/${postId}`);
+
+            dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
+        }
+        catch (err) {
+            dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+        }
+    }
+
+}
+
+// Unlike a post
+
+export const removeLike = (postId) => {
+    return async (dispatch) => {
+
+        try {
+            const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+            dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
         }
         catch (err) {
             dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,5 +1,5 @@
 
-import { GET_POSTS, POST_ERROR } from '../actions/types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -14,6 +14,8 @@ export default (state = initialState, action) => {
             return {...state, posts: action.payload, loading: false};
         case POST_ERROR:
             return {...state, error: action.payload, loading: false};
+        case UPDATE_LIKES: // map through all posts and find the post which we need to update. After we get the post, just update the likes array inside of it.
+            return {...state, posts: state.posts.map((post) => post._id === action.payload.postId ? {...post, likes: action.payload.likes} : post), loading: false }
         default:
             return state;
     }
